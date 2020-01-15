7f22b32cdb54c59b8a2e16324ee0620297519c0c
Jesse Mu <6245103+jayelm@users.noreply.github.com>
2020-01-15 13:16:34 -0800

    More control to clean.py

--- clean.py
+++ clean.py
@@ -3,7 +3,18 @@ from subprocess import call
 from glob import glob
 
 if __name__ == '__main__':
-    for ext in ['csv', 'npy', 'mmap']:
+    from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
+
+    parser = ArgumentParser(
+        description='Clean experiment file',
+        formatter_class=ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('extensions', nargs='*', default=['csv', 'npy', 'mmap'],
+                        help='Which extensions to remove')
+
+    args = parser.parse_args()
+
+    for ext in args.extensions:
         files = glob(f'{OUTPUT_FOLDER}/*.{ext}')
         if files:
             cmd = ['rm', *files]
