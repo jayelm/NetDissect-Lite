ae918902024c07eb72b4547a42996e2f0d3034f3
Jesse Mu <6245103+jayelm@users.noreply.github.com>
2020-02-14 15:32:52 -0800

    Add missing categories

--- script/save_ade_labels.py
+++ script/save_ade_labels.py
@@ -30,7 +30,14 @@ if __name__ == '__main__':
             for scene in os.listdir(l_dir):
                 scene_dir = os.path.join(l_dir, scene)
                 for img in os.listdir(scene_dir):
-                    if img.endswith('.jpg'):
+                    maybe_new_scene_dir = os.path.join(scene_dir, img)
+                    if os.path.isdir(maybe_new_scene_dir):
+                        # Possible to go two deep
+                        new_scene = f'{scene}-{img}'
+                        for subimg in os.listdir(maybe_new_scene_dir):
+                            if subimg.endswith('.jpg'):
+                                img2scene[subimg] = new_scene
+                    elif img.endswith('.jpg'):
                         img2scene[img] = scene
 
     with open('dataset/broden1_224/ade20k_scenes.json', 'w') as f:
