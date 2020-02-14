Add hover masks, fix off-by-one viz

visualize/report/final.py
@@ -17,10 +17,12 @@ import warnings
 from tqdm import tqdm
 import loader.data_loader.formula as F
 import os
+import shutil
 
 
-def generate_final_layer_summary(ds, weight, prev_layername=None, prev_tally=None, contributors=None):
+def generate_final_layer_summary(ds, weight, last_features, last_thresholds, last_preds, last_logits, prev_layername=None, prev_tally=None, contributors=None):
     ed = expdir.ExperimentDirectory(settings.OUTPUT_FOLDER)
+    ed.ensure_dir('html', 'image', 'final')
 
     html_fname = ed.filename(f"html/{expdir.fn_safe('final')}.html")
 
@@ -41,6 +43,7 @@ def generate_final_layer_summary(ds, weight, prev_layername=None, prev_tally=Non
                 f'</div>'
                 f'<div class="card-body">'
         )
+        all_contrs = []
         for contr_i, contr_name in enumerate(sorted(list(contributors.keys()))):
             contr_dict = contributors[contr_name]
             if contr_dict['contr'][0] is None:
@@ -48,24 +51,61 @@ def generate_final_layer_summary(ds, weight, prev_layername=None, prev_tally=Non
             contr, inhib = contr_dict['contr']
             contr = np.where(contr[cl])[0]
             contr_weights = contr_dict['weight'][cl, contr]
+            all_contrs.extend(contr)
 
             inhib = np.where(inhib[cl])[0]
             inhib_weights = contr_dict['weight'][cl, inhib]
 
             contr_url_str = ','.join(map(str, contr))
-            contr_label_str = ', '.join(f'{u} ({prev_tally.get(u, "unk")}, {w:.3f})' for u, w in zip(contr, contr_weights))
+            contr_labels = [f'{u + 1} ({prev_tally.get(u + 1, "unk")}, {w:.3f})' for u, w in zip(contr, contr_weights)]
+            contr_labels = [f'<span class="label contr-label" data-unit="{u}" data-clname="{cl_name}">{l}</span>' for u, l in zip(contr, contr_labels)]
 
             inhib_url_str = ','.join(map(str, inhib))
-            inhib_label_str = ', '.join(f'{u} ({prev_tally.get(u, "unk")}, {w:.3f})' for u, w in zip(inhib, inhib_weights))
+            inhib_labels = [f'{u + 1} ({prev_tally.get(u + 1, "unk")}, {w:.3f})' for u, w in zip(inhib, inhib_weights)]
+            inhib_labels = [f'<span class="label inhib-label" data-unit="{u}" data-clname="{cl_name}">{l}</span>' for u, l in zip(inhib, inhib_labels)]
+
+            contr_label_str = ', '.join(contr_labels)
+            inhib_label_str = ', '.join(inhib_labels)
 
             html.append(
                 f'<p class="contributors"><a href="{prev_layername}.html?u={contr_url_str}">Contributors ({contr_name}): {contr_label_str}</a></p>'
                 f'<p class="inhibitors"><a href="{prev_layername}.html?u={inhib_url_str}">Inhibitors ({contr_name}): {inhib_label_str}</a></p>'
             )
-        html.append(
-                f'</div>'
-            f'</div>'
-        )
+
+        all_contrs = list(set(all_contrs))
+        # Save images with highest logits
+        cl_images = [i for i in range(len(last_features)) if f"{ds.scene(i)}-s" == cl_name]
+        cl_images = sorted(cl_images, key=lambda i: last_logits[i, cl], reverse=True)
+        if not cl_images:
+            continue
+        for i, im_index in enumerate(cl_images[:5]):
+            #  breakpoint()
+            imfn = ds.filename(im_index)
+            imfn_base = os.path.basename(imfn)
+            html_imfn = ed.filename(f"html/image/final/{imfn_base}")
+            shutil.copy(imfn, html_imfn)
+            html.append(
+                #  f'<canvas class="mask-canvas" id="{cl_name}-{i}" width="100" height="100" data-src="">Your browser does not support canvas</canvas>"'
+                f'<img loading="lazy" class="final-img" id="{cl_name}-{i}" data-clname="{cl_name}" width="100" height="100" data-imfn="{imfn_base}" src="image/final/{imfn_base}">'
+            )
+            # Save masks
+            for unit in all_contrs:
+                imfn_alpha = imfn_base.replace('.jpg', '.png')
+                #  breakpoint()
+                feats = last_features[im_index, unit]
+                thresh = last_thresholds[unit]
+                mask = (feats > thresh).astype(np.uint8) * 255
+                mask = np.clip(mask, 50, 255)
+                mask = Image.fromarray(mask).resize((settings.IMG_SIZE, settings.IMG_SIZE), resample=Image.BILINEAR)
+                # All black
+                mask_alpha = Image.fromarray(np.zeros((settings.IMG_SIZE, settings.IMG_SIZE), dtype=np.uint8), mode='L')
+                mask_alpha.putalpha(mask)
+                mask_fname = ed.filename(f"html/image/final/mask-{unit}-{imfn_alpha}")
+                mask_alpha.save(mask_fname)
+                # Upscale mask...save asa
+
+        html.append(f'</div></div>')
+        html.append('<!-- br -->')
 
     html.append(html_common.HTML_SUFFIX);
     with open(html_fname, 'w') as f:

visualize/report/neuron.py
@@ -298,11 +298,12 @@ def generate_html_summary(ds, layer, preds, mc, maxfeature=None, features=None, 
                 continue
             contr, inhib = contr_dict['contr']
 
-            contr = np.where(contr[unit])[0]
+            # Increment by 1 since prev_tally starts at 1
+            contr = np.where(contr[unit])[0] + 1
             contr_label_str = ', '.join(f'{u} ({prev_tally.get(u, "unk")})' for u in contr)
             contr_url_str = ','.join(map(str, contr))
 
-            inhib = np.where(inhib[unit])[0]
+            inhib = np.where(inhib[unit])[0] + 1
             inhib_url_str = ','.join(map(str, inhib))
             inhib_label_str = ', '.join(f'{u} ({prev_tally.get(u, "unk")})' for u in inhib)
 
@@ -336,15 +337,15 @@ def generate_html_summary(ds, layer, preds, mc, maxfeature=None, features=None, 
             contr_str +
             '</div>')
         html.append(
-            '<div class="thumbcrop"><img src="%s" height="%d"></div>' %
+            '<div class="thumbcrop"><img loading="lazy" src="%s" height="%d"></div>' %
             (imfn, imscale))
         html.append('<p class="midrule">Other examples of feature (<span class="bluespan">feature mask</span> <span class="redspan">unit mask</span>)</p>')
         html.append(
-            '<div class="thumbcrop"><img src="%s" height="%d"></div>' %
+            '<div class="thumbcrop"><img loading="lazy" src="%s" height="%d"></div>' %
             (row2fn, imscale))
         html.append(f'<p class="midrule">Examples of {neglab}</p>')
         html.append(
-            '<div class="thumbcrop"><img src="%s" height="%d"></div>' %
+            '<div class="thumbcrop"><img loading="lazy" src="%s" height="%d"></div>' %
             (row3fn, imscale))
         html.append('</div') # Leave off > to eat spaces
     html.append('></div>')