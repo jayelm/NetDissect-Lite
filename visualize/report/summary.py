50fb90bd0df5785f4fcc3c19bc6b76649dbb681c
Jesse Mu <6245103+jayelm@users.noreply.github.com>
2020-02-27 19:08:41 -0800

    Add better wordnet summaries

--- visualize/report/summary.py
+++ visualize/report/summary.py
@@ -18,6 +18,19 @@ def filter_oov(nlps):
     return [n for n in nlps if not (n.vector == 0.0).all()]
 
 
+def clean(leaves, to_underscore=False):
+    if to_underscore:
+        leaves = [l.replace('-', '_') for l in leaves]
+        leaves = [l[:-2] if l.endswith('_s') else l for l in leaves]
+        leaves = [l[:-2] if l.endswith('_c') else l for l in leaves]
+    else:
+        leaves = [l.replace('-', ' ').replace('_', ' ') for l in leaves]
+        leaves = [l[:-2] if l.endswith(' s') else l for l in leaves]
+        leaves = [l[:-2] if l.endswith(' c') else l for l in leaves]
+    # Remove scene suffixes
+    return leaves
+
+
 def get_nlps(f, namer, vals=False):
     """
     Get spacy nlp objects for leaves in formula, with some cleanup
@@ -27,10 +40,7 @@ def get_nlps(f, namer, vals=False):
     else:
         leaves = f
     leaves = [namer(l).lower() for l in leaves]
-    leaves = [l.replace('-', ' ').replace('_', ' ') for l in leaves]
-    # Remove scene suffixes
-    leaves = [l[:-2] if l.endswith(' s') else l for l in leaves]
-    leaves = [l[:-2] if l.endswith(' c') else l for l in leaves]
+    leaves = clean(leaves)
     return [nlp(l) for l in leaves]
 
 
@@ -109,6 +119,61 @@ def get_synset(t):
         return None
 
 
+PATH_TYPES = [
+    'hypernyms',
+    'instance_hypernyms',
+    'hyponyms',
+    'instance_hyponyms',
+    'member_meronyms',
+    'part_meronyms',
+    'substance_meronyms',
+    'similar_tos',
+    'also_sees',
+]
+
+
+def find_path(src, dest, path_types=PATH_TYPES):
+    """
+    Find path between wordnet synsets
+    """
+    visited = set()
+
+    # Create a queue for BFS
+    queue = []
+
+    queue.append((src, tuple()))
+    visited.add(src)
+
+    while queue:
+        # Dequeue a vertex from queue
+        n, path = queue.pop(0)
+
+        # If this adjacent node is the destination node,
+        # then return true
+        if n == dest:
+             return path
+
+        #  Else, continue to do BFS
+        relations = set()
+        for path_type in path_types:
+            relations.update(getattr(n, path_type)())
+        for i in relations:
+            if i not in visited:
+                new_path = path + (n, )
+                queue.append((i, new_path))
+                visited.add(i)
+
+    return None
+
+
+def wn_midpoint(src, dest):
+    path = find_path(src, dest)
+    if path is None:
+        return None
+    midp = len(path) // 2
+    return path[midp]
+
+
 def wn_summarize(f, namer):
     """
     Get one-word summary of label which is closest in embedding space
@@ -116,16 +181,22 @@ def wn_summarize(f, namer):
     """
     leaves = f.get_vals()
     leaves = [namer(l).lower() for l in leaves]
-    leaves = [l.replace('-', ' ').replace('_', ' ') for l in leaves]
-    # Remove scene suffixes
-    leaves = [l[:-2] if l.endswith('-s') else l for l in leaves]
+    leaves = clean(leaves, to_underscore=True)
 
-    ss = []
-    for l in leaves:
-        s = get_synset(l)
-        if s is None:
+    synsets = [get_synset(s) for s in leaves]
+    if all(s is None for s in synsets):
+        return 'unk', 0.0
+    synsets = [s for s in synsets if s is not None]
+
+    if len(synsets) == 1:
+        return str(synsets[0])
+    # Find midpoint between each pair, then find midpoints of the midpoints
+    while len(synsets) > 1:
+        a = synsets.pop(0)
+        b = synsets.pop(0)
+        midp = wn_midpoint(a, b)
+        if midp is None:
             return 'unk', 0.0
-        ss.append(s)
-    the_ss = functools.reduce(lambda a, b: a.lowest_common_hypernyms(b)[0], ss)
+        synsets.append(midp)
 
-    return str(the_ss)
+    return str(synsets[0])
